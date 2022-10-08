8fd1a166f5dd2a4404e0d6b31b51f84f61a8c1c7
ytzys <ytzys.163.com>
2022-10-08 17:25:32 +0800

    modify

--- src/com/ytzys/shikangweibobackup/Backup.java
+++ src/com/ytzys/shikangweibobackup/Backup.java
@@ -9,11 +9,15 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -27,12 +31,13 @@ public class Backup {
     public static String editHistryBaseUrl = "https://weibo.com/ajax/statuses/editHistory?page=1&mid=";
     public static String longTextBaseUrl = "https://weibo.com/ajax/statuses/longtext?id=";
 
-    public static void main(String[] args) {
+    static SimpleDateFormat sdf1 = new SimpleDateFormat("E MMM dd HH:mm:ss z yyyy", Locale.US);
+    static SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss E", Locale.US);
 
+    public static void main(String[] args) {
 
         Executor executor = Executors.newSingleThreadExecutor();
         Runnable task = new Runnable() {
-
 
             @Override
             public void run() {
@@ -47,57 +52,48 @@ public class Backup {
                     if (!dir.exists()) {
                         dir.mkdir();
                     }
-//                    ListDataBean data = JsonMapper.INSTANCE.fromJson(tmp, ListDataBean.class);
                     for (int i = 0; i < data.data.list.size(); i++) {
                         ListDataBean.DataEntity.ListEntity listEntity = data.data.list.get(i);
-                        String name = listEntity.created_at.replaceAll("[ :]", "_");
+                        String name = getFileName(listEntity.created_at);
                         File file = new File(dir.getPath() + "/" + name + ".txt");
                         if (!file.exists()) {
                             System.out.println(i);
-//                            String string = getString(detailBaseUrl + listEntity.mblogid);
-//                            DetailBean detailBean = null;
-//                            try {
-//                                detailBean = JSON.parseObject(string, DetailBean.class);
-//                                String text = detailBean.data.text;
-//                                BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-//                                writer.write(text);
-//                                writer.close();
-//                            } catch (Exception e) {
-//                                System.out.println(listEntity.mblogid + " >>> " + listEntity.text);
-                            try {
-                                LongTextBean longTextBean = JSON.parseObject(getString(longTextBaseUrl + listEntity.mblogid), LongTextBean.class);
-                                BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-                                writer.write(longTextBean.data.longTextContent);
+                            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+                            // is long weibo
+                            if (listEntity.isLongText) {
+                                String url = longTextBaseUrl + listEntity.mblogid;
+                                try {
+                                    String res = getString(url);
+                                    LongTextBean longTextBean = JSON.parseObject(res, LongTextBean.class);
+                                    writer.write(longTextBean.data.longTextContent);
+                                    writer.close();
+                                } catch (Exception e1) {
+                                    System.out.println(url + " ;Get long text failed.");
+                                }
+                            } else {
+                                writer.write(listEntity.text);
                                 writer.close();
-                            } catch (Exception e1) {
-                                System.out.println("Get long text failed.");
                             }
-//                            }
+                            // handle edited weibo
                         } else {
                             if (listEntity.edit_count >= 1) {
                                 String string = getString(editHistryBaseUrl + listEntity.mid);
                                 EditHistoryBean editHistoryBean = JSON.parseObject(string, EditHistoryBean.class);
                                 List<EditHistoryBean.StatusesEntity> statuses = editHistoryBean.statuses;
-                                for (int j = 0; j < statuses.size(); j++) {
-                                    String text = statuses.get(j).text;
-                                    String fileStr = name + "_" + statuses.get(j).created_at.replaceAll("[ :]", "_");
-                                    File file1 = new File(dir.getPath() + "/" + fileStr + ".txt");
-                                    if (!file1.exists()) {
-                                        BufferedWriter writer = new BufferedWriter(new FileWriter(file1));
-                                        writer.write(text);
-                                        writer.close();
+                                if (statuses != null)
+                                    for (int j = 0; j < statuses.size(); j++) {
+                                        String text = statuses.get(j).text;
+//                                        String fileStr = name + "_" + statuses.get(j).created_at.replaceAll("[ :]", "_");
+                                        String fileStr = name + "_" + getFileName(statuses.get(j).created_at);
+                                        File file1 = new File(dir.getPath() + "/" + fileStr + ".txt");
+                                        if (!file1.exists()) {
+                                            BufferedWriter writer = new BufferedWriter(new FileWriter(file1));
+                                            writer.write(text);
+                                            writer.close();
+                                        }
                                     }
-                                }
                             }
                         }
-//                        Date date = new Date(listEntity.created_at);
-//                        if (date.compareTo(new Date("Wed Mar 02 16:12:57 +0800 2022")) > 0) {
-//                            System.out.println(listEntity.created_at);
-//                            JOptionPane.showMessageDialog(null, "we got it",
-//                                    "result", JOptionPane.INFORMATION_MESSAGE);
-//                            break;
-//                        }
-
                     }
 
 
@@ -120,14 +116,23 @@ public class Backup {
         }
     }
 
+    // get file name, using the create time, file name format: yyyy-MM-dd HH-mm-ss E
+    private static String getFileName(String created_at) {
+        try {
+            return sdf2.format(sdf1.parse(created_at));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return "error";
+        }
+    }
+
     private static String getString(String urlStr) throws IOException {
         String tmp = "";
         URL url = new URL(urlStr);
         byte[] buffer = new byte[1024 * 8];
         int readSize;
-        HttpURLConnection conn = (HttpURLConnection) url
-                .openConnection();
-        conn.setRequestProperty("Cookie", "SSOLoginState=1602124982; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WF-lBYf6QcxT_0oJh.kv3dw5JpX5KMhUgL.Foz7e0qRS0-41K-2dJLoIEXLxKqLBozL1hnLxK.L1KnLBoeLxKBLBonLBoBLxKqLBo2L1-qLxKBLB.zL1K.t; _s_tentry=login.sina.com.cn; Apache=8247843021384.305.1646184153171; SINAGLOBAL=8247843021384.305.1646184153171; ULV=1646184153215:1:1:1:8247843021384.305.1646184153171:; XSRF-TOKEN=HukLs4DJDbQYzfbzdflz7kG8; UOR=login.sina.com.cn,weibo.com,www.google.com; SCF=AtnWa351Jh6ZHltUaoVwCzxYld90sKX-uehoVN6Z0SoDMll1VMKYvAUH7ryH8SxgQ3NDPHFM-rOf-qGB0Tl4xCw.; SUB=_2A25PIS9EDeRhGeRO6FQZ9yvFwjmIHXVsVweMrDV8PUNbmtANLWb9kW9NUG1qmTivz6HT7UqOu-DuMa9z0s1nZXqD; ALF=1678152337; WBPSESS=w6PQnJHNETvGmhGCZzFKLZIxY7thbUoQg_TAyNL7u2nRaexaUTr-4l94SHuy227PhZ3l0SPY8czp7XRZCgnqer6JceiSR4qSFaTMvabKQjF5FG0-6ixqtxQgQ5VsNWX6");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestProperty("Cookie", "XSRF-TOKEN=iqzG9COP6KgxpBFhPhzK0qe3; _s_tentry=weibo.com; Apache=3192130945781.428.1665200599512; SINAGLOBAL=3192130945781.428.1665200599512; ULV=1665200599523:1:1:1:3192130945781.428.1665200599512:; SUB=_2A25ORXLXDeRhGeRO6FQZ9yvFwjmIHXVtM-MfrDV8PUNbmtAKLRTEkW9NUG1qmUxwEGAbbB-S0P4c0CH2RSMvpppb; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WF-lBYf6QcxT_0oJh.kv3dw5JpX5KzhUgL.Foz7e0qRS0-41K-2dJLoIEXLxKqLBozL1hnLxK.L1KnLBoeLxKBLBonLBoBLxKqLBo2L1-qLxKBLB.zL1K.t; ALF=1696740870; SSOLoginState=1665204871; WBPSESS=w6PQnJHNETvGmhGCZzFKLVgYU04Zkm35mgyIf2do9gfZBKS0Ne-c04LWmA545h0-Bt2IJYHHwecSYrAcpclMeNIMFFCVYPQcX8gd7DX030ovPee8Z2_lVPN3mYEBl_plvnn1ggfbwq62Yg2xIiaDeQ==");
         conn.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36");
         if (conn.getResponseCode() == 200) {
             InputStream is = conn.getInputStream();
